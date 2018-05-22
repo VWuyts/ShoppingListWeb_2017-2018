9dcc4cd86d4d06820263e04323e2102d37e3fd81
Veronique Wuyts <veroniquewuyts@skynet.be>
2018-05-22 20:55:53 +0200

    Initiate home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+/* Ref: */
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReferencesComponent } from './references/references.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'referenties', component: ReferencesComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 
 @NgModule({
@@ -13,10 +22,13 @@ import { ReferencesComponent } from './references/references.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ReferencesComponent
+    ReferencesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
