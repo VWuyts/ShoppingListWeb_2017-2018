c59cffc91382c4d2e8664f35c404afb020c29d36
Veronique Wuyts <veroniquewuyts@skynet.be>
2018-06-01 20:24:04 +0200

    Created data services

---
+++ src/app/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  public name: string;
+  public category: string;
+  public amount: number;
+  public unit: string;
+  public image: string;
+  public note: string;
+  public shop: string;
+  public isFixedShop: boolean;
+  public inPromotion: boolean;
+}
